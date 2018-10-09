8fa3497db303faa48d6d3748f097a5cff274793c
Quinn Bush <qbush@indiana.edu>
2018-10-09 19:49:25 -0400

    update

--- public/api/work.php
+++ public/api/work.php
@@ -7,15 +7,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   exit;
 }
 
-$task_Id = intval($_GET['taskId'] ?? 0);
+$taskId = intval($_GET['taskId'] ?? 0);
 
-if ($task_Id < 1) {
+if ($taskId < 1) {
   throw new Exception('Invalid Task ID');
 }
 
 
 // 1. Go to the database and get all work associated with the $taskId
-$workArr = Work::getWorkByTaskId($task_Id);
+$workArr = Work::getWorkByTaskId($taskId);
 
 // 2. Convert to JSON
 $json = json_encode($workArr, JSON_PRETTY_PRINT);
